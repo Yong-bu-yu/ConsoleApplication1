172e2bfff459c89169b60e5483be629058f2b05e
Yong-bu-yu <47913054+Yong-bu-yu@users.noreply.github.com>
2019-06-08 16:12:53 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实验
+{
+    class Program
+    {
+        static void PrintKeys(RegistryKey rkey)
+        {
+            //调用RegistryKey类的方法GetSubKeyNames获取注册表中各项的名称
+            String[] names = rkey.GetSubKeyNames();
+            int icount = 0;
+            Console.WriteLine("Subkeys of " + rkey.Name);
+            Console.WriteLine("-------------------------------------------------------");
+            foreach(String s in names)
+            {
+                Console.WriteLine(s);
+                icount++;
+                if (icount >= 9)
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            RegistryKey rk = Registry.ClassesRoot;
+            PrintKeys(rk);
+        }
+    }
+}
