dd8c258b31db9cf249892d523dfee9b2075f25ea
Yong-bu-yu <47913054+Yong-bu-yu@users.noreply.github.com>
2019-06-23 19:55:04 +0800

    Stream Writer
    关闭当前的StreamWriter和基础流。Close的此实现调用传递true值的Dispose方法。必须调用Close以确保所有数据正确写出到基础流中。调用Close后，在StreamWriter上进行任何操作均可能引发异常。除非显式地调用Flush或Close，否则刷新时不会刷新其基础编码器。将AutoFlush设置为true意味着将数据从缓冲区刷新到流中，但不刷新编码器状态。这将允许编码器保持其状态（不完全字符），以便它可以正确地对下一个字符块进行编码。此方案影响UTF8和UTF7，这二者中，某些字符只能在编码器收到相邻的一个或多个字符后才能进行编码。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实验
+{
+    class Program
+    {
+            public static void WriteLineTypes(Stream sink)
+            {
+                //创建一个输出流
+                StreamWriter sw = new StreamWriter(sink);
+                sw.WriteLine(true);
+                sw.WriteLine(false);
+                sw.WriteLine((byte)7);
+                sw.WriteLine('z');
+                sw.WriteLine(new char[] { 'A', 'B', 'C', 'D' });
+                sw.WriteLine(new Decimal(123.45));
+                sw.WriteLine((short)212);
+                sw.WriteLine((long)212);
+                sw.WriteLine("{0} : {1}", "string formatting supported", "true");
+                sw.Close();
+            }
+        public static void Main(string[] args)
+        {
+            //以创建文件模式打开文件流
+            Stream fStream = new FileStream("E:\\1\\实验\\实验\\textio.dat", FileMode.Create);
+            WriteLineTypes(fStream);
+            fStream.Close();
+         }
+    }
+}
