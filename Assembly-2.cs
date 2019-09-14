1f69ce4bb386ed1f15cbcd984f7e1305f2d4f0d8
Yong-bu-yu <47913054+Yong-bu-yu@users.noreply.github.com>
2019-09-14 22:07:00 +0800

    Assembly-2
    说明：本示例说明如何利用Assembly类来检查一个程序集中包含的类型。这个程序集可以是一个可执行文件，甚至是一个包含在系统内的动态链接库。在这个例子中，我们用它获得程序的名称，在名称后面加上.exe，就可以检查程序集；也可以简单地在应用程序中加入代码。

---
+++ Assembly-2.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace 实验
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //获取当前正在运行的进程
+            Process p = Process.GetCurrentProcess();
+            //获取正在运行的进程名称
+            string assemblyName = p.ProcessName + ".exe";
+            Console.WriteLine("Examining:{0}", assemblyName);
+            //把正在运行的进程加入装配类中
+            Assembly a = Assembly.LoadFrom(assemblyName);
+            Type[] types = a.GetTypes();
+            //输出所有的装配类中的类名称
+            foreach (Type t in types)
+            {
+                Console.WriteLine("\nType :{0}", t.FullName);
+                Console.WriteLine("\nBase class :{0}", t.BaseType.FullName);
+            }
+        }
+    }
+}
