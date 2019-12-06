119021b6bf36f7d93d95800caf46df32021d7ddc
Yong-bu-yu <47913054+Yong-bu-yu@users.noreply.github.com>
2019-12-06 20:41:45 +0800

    Update 展开或隐藏菜单.cs
    展开或隐藏菜单加系统托盘菜单

--- 展开或隐藏菜单.cs
+++ 展开或隐藏菜单.cs
@@ -27,13 +27,33 @@ namespace WindowsFormsApplication2
         protected override void OnLoad(EventArgs e)
         {
             展开关闭其他项ToolStripMenuItem.Click += 展开关闭其他项ToolStripMenuItem_Click;
+            隐藏ToolStripMenuItem.Click += 隐藏ToolStripMenuItem_Click;
+            显示ToolStripMenuItem.Click += 显示ToolStripMenuItem_Click;
+            退出ToolStripMenuItem.Click += 退出ToolStripMenuItem_Click;
             员工录入ToolStripMenuItem.Visible = false;
             添加用户ToolStripMenuItem.Visible = false;
             设置密码ToolStripMenuItem.Visible = false;
             修改密码ToolStripMenuItem.Visible = false;
             忘记密码ToolStripMenuItem.Visible = false;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
             base.OnLoad(e);
         }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+
+        private void 隐藏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visible = false;
+        }
+
         bool flag = true;
         private void 展开关闭其他项ToolStripMenuItem_Click(object sender, EventArgs e)
         {
